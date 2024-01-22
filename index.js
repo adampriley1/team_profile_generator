65da616fb8a0e464508b67e68bc0b824a632de9a
adampriley1 <adampriley1@hotmail.com>
2024-01-22 09:50:59 +0000

    working on code to get data from user and output to html

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
@@ -13,3 +14,145 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// array of questions for user 
+const initialQuestions = 
+    [
+      {
+        type: 'input',
+        name: 'getName',
+        message: 'What is the team manager name?',
+      },
+        {
+          type: 'input',
+          name: 'getId',
+          message: 'What is the Employee ID?',
+        },
+        {
+          type: 'input',
+          name: 'getEmail',
+          message: 'What is manager email?',
+        },
+        {
+          type: 'input',
+          name: 'getOfficeNumber',
+          message: 'What is the Office number?',
+        },
+       
+    ]
+
+    const menu = [
+      {
+        type: 'list',
+        name: 'addMore',
+        message: 'Do you want to..',
+        choices: ['Add engineer', 'Add intern', 'Finish building the team'],
+      },
+    ]
+
+    //engineer questions 
+    const engineerQuestions = 
+    [
+        {
+          type: 'input',
+          name: 'getName',
+          message: 'Enter engineers name',
+        },
+        {
+          type: 'input',
+          name: 'getId',
+          message: 'Enter engineers ID',
+        },
+        {
+          type: 'input',
+          name: 'getEmail',
+          message: 'Enter engineers email',
+        },
+        {
+          type: 'input',
+          name: 'getGithub',
+          message: 'Enter engineers Github username',
+        },
+];
+
+
+//intern questions
+const internQuestions = 
+[
+    {
+      type: 'input',
+      name: 'getName',
+      message: 'Enter interns name',
+    },
+    {
+      type: 'input',
+      name: 'getId',
+      message: 'Enter interns ID',
+    },
+    {
+      type: 'input',
+      name: 'getmail',
+      message: 'Enter interns email',
+    },
+    {
+      type: 'input',
+      name: 'getSchool',
+      message: 'Enter interns School',
+    },
+  
+];
+
+//output data array 
+const output = [];
+
+//Initiilise the programme 
+function init() {
+  inquirer.prompt(initialQuestions).then((answers) => {
+    output.push(answers);
+    selected();
+    then(writeToHTML);
+  });
+}
+//2nd set of questions based on who is selected
+function selected(){
+    inquirer.prompt(menu).then((answers) => {
+      if (answers.addMore === 'Add engineer') {
+        engineer();
+      } else if (answers.addMore === 'Add intern') {
+        intern();
+      } else if (answers.addMore === 'Finish building the team') {
+        console.log('Your team profile:', output);
+      }
+    
+    
+    });
+  }
+
+function engineer() {
+  inquirer.prompt(engineerQuestions).then((answers) => {
+    output.push(answers);
+    selected();
+  });
+}
+
+function intern() {
+  inquirer.prompt(internQuestions).then((answers) => {
+    output.push(answers);
+    selected();
+  });
+}
+
+init();
+
+// function to write README file //this is from last week assignment - modify
+function writeToHTML(answers) {
+    const addManager = generateManager(answers);
+    const addEngineer = generateEngineer(answers);
+    const addIntern = enerateIntern(answers);
+    
+      fs.appendFile('page-template.js',addManager, addEngineer, addIntern, (err) =>
+  err ? console.error(err) : console.log('Successfully added team member')
+      )
+  };
+  
+
+  

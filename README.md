2beb009fc45af9c71e2d86cb50a0c0172b5660e0
adampriley1 <adampriley1@hotmail.com>
2024-01-23 13:23:24 +0000

    added readme file

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # team_profile_generator
+
+Description
+The challenge was to build a Team Profile generator using node, Inquirer and testing with Jest
+
+Requirements:
+To take the given starter code and convert it into a working Node.js command-line application. This application will take in information about employees on a software engineering team, then generates an HTML webpage that displays summaries for each person. I was provided with tests, to allow me to ensure every part of the code passes each provided test.
+
+When the user type node index.js into the command line. They will be prompted to add a manager. They will then have options to add multiple engineers and . or interns to the team.
+
+
+
+
+Screenshot of working in terminal:
+
+
+
+Video Demonstration:
+https://vimeo.com/902920946/a8fa26d345?share=copy
+
+## Installation
+Open Intergrated terminal from the ReadmeGenerator folder
+
+
+Application Link:
+
+https://adampriley1.github.io/ReadmeGenerator/
+
+## Usage
+
+To run, type 'node index.js' into terminal.
+Answer questions, README file then produced with the name APP_README.md
+
+
+
+## Credits
+
+Adam Riley 
+
+## License
+MIT License
+
+Copyright (c) [2023] [Adam Riley]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+---
--- index.js
+++ index.js
@@ -5,136 +5,127 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
-
-// array of questions for user 
-const initialQuestions = 
-    [
-      {
-        type: 'input',
-        name: 'getName',
-        message: 'What is the team manager name?',
-      },
-        {
-          type: 'input',
-          name: 'getId',
-          message: 'What is the Employee ID?',
-        },
-        {
-          type: 'input',
-          name: 'getEmail',
-          message: 'What is manager email?',
-        },
-        {
-          type: 'input',
-          name: 'getOfficeNumber',
-          message: 'What is the Office number?',
-        },
-       
-    ]
-
-    const menu = [
-      {
-        type: 'list',
-        name: 'addMore',
-        message: 'Do you want to..',
-        choices: ['Add engineer', 'Add intern', 'Finish building the team'],
-      },
-    ]
-
-    //engineer questions 
-    const engineerQuestions = 
-    [
-        {
-          type: 'input',
-          name: 'getName',
-          message: 'Enter engineers name',
-        },
-        {
-          type: 'input',
-          name: 'getId',
-          message: 'Enter engineers ID',
-        },
-        {
-          type: 'input',
-          name: 'getEmail',
-          message: 'Enter engineers email',
-        },
-        {
-          type: 'input',
-          name: 'getGithub',
-          message: 'Enter engineers Github username',
-        },
+// array of questions for user
+const initialQuestions = [
+  {
+    type: "input",
+    name: "getName",
+    message: "What is the team manager name?",
+  },
+  {
+    type: "input",
+    name: "getId",
+    message: "What is the Employee ID?",
+  },
+  {
+    type: "input",
+    name: "getEmail",
+    message: "What is manager email?",
+  },
+  {
+    type: "input",
+    name: "getOfficeNumber",
+    message: "What is the Office number?",
+  },
 ];
 
+const menu = [
+  {
+    type: "list",
+    name: "addMore",
+    message: "Do you want to..",
+    choices: ["Add engineer", "Add intern", "Finish building the team"],
+  },
+];
+
+//engineer questions
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "getName",
+    message: "Enter engineers name",
+  },
+  {
+    type: "input",
+    name: "getId",
+    message: "Enter engineers ID",
+  },
+  {
+    type: "input",
+    name: "getEmail",
+    message: "Enter engineers email",
+  },
+  {
+    type: "input",
+    name: "getGithub",
+    message: "Enter engineers Github username",
+  },
+];
 
 //intern questions
-const internQuestions = 
-[
-    {
-      type: 'input',
-      name: 'getName',
-      message: 'Enter interns name',
-    },
-    {
-      type: 'input',
-      name: 'getId',
-      message: 'Enter interns ID',
-    },
-    {
-      type: 'input',
-      name: 'getmail',
-      message: 'Enter interns email',
-    },
-    {
-      type: 'input',
-      name: 'getSchool',
-      message: 'Enter interns School',
-    },
-  
+const internQuestions = [
+  {
+    type: "input",
+    name: "getName",
+    message: "Enter interns name",
+  },
+  {
+    type: "input",
+    name: "getId",
+    message: "Enter interns ID",
+  },
+  {
+    type: "input",
+    name: "getmail",
+    message: "Enter interns email",
+  },
+  {
+    type: "input",
+    name: "getSchool",
+    message: "Enter interns School",
+  },
 ];
 
-//output data array 
+//output data array
 const output = [];
 
-//Initiilise the programme 
+//Initiilise the programme
 function init() {
   inquirer.prompt(initialQuestions).then((answers) => {
-    output.push(answers); 
+    output.push(answers);
     //run selected function to ask menu question
     selected();
-  })
+  });
   // .then(()=> render(output));
 }
 //2nd set of questions based on who is selected
-function selected(){
-    inquirer.prompt(menu).then((answers) => {
-      if (answers.addMore === 'Add engineer') {
-        engineer();
-      } else if (answers.addMore === 'Add intern') {
-        intern();
-      } else if (answers.addMore === 'Finish building the team') {
-        console.log('Your team profile has been built');
-        inquirer.prompt([]).then(() => render(output));
-      }
-    });
-  }
+function selected() {
+  inquirer.prompt(menu).then((answers) => {
+    if (answers.addMore === "Add engineer") {
+      engineerQs();
+    } else if (answers.addMore === "Add intern") {
+      internQs();
+    } else if (answers.addMore === "Finish building the team") {
+      console.log("Your team profile has been built");
+      console.log(manager);
+      render(output);
+    }
+  });
+}
 
-function engineer() {
+function engineerQs() {
   inquirer.prompt(engineerQuestions).then((answers) => {
     output.push(answers);
     selected();
   });
 }
 
-function intern() {
+function internQs() {
   inquirer.prompt(internQuestions).then((answers) => {
     output.push(answers);
     selected();
@@ -142,6 +133,3 @@ function intern() {
 }
 
 init();
-
-
-  
